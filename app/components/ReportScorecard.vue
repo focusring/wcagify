a65a3c88ac648d7c6dfd18e3b447e8bea7f6181f
timdamen <tim.damen80@gmail.com>
2026-02-23 09:46:29 +0100

    feat: ensure scorecard table is semantically correct
    Replace definition list with native HTML table using proper thead,
    tbody, tfoot, th[scope], and visually hidden caption for accessibility.

--- app/components/ReportScorecard.vue
+++ app/components/ReportScorecard.vue
@@ -16,21 +16,48 @@ const principles = ['perceivable', 'operable', 'understandable', 'robust'] as co
 </script>
 
 <template>
-  <div class="space-y-2">
+  <div class="space-y-4">
     <p class="text-lg font-medium text-gray-950 dark:text-white">
       {{ t('report.conformanceLevel', { level: targetLevel, conforming: data.conforming.all, total: data.totals.all }) }}
     </p>
-    <dl class="mt-4 space-y-1 text-sm text-gray-700 dark:text-gray-300">
-      <div
-        v-for="principle in principles"
-        :key="principle"
-        class="flex gap-1"
-      >
-        <dt class="after:content-[':']">
-          {{ t(`report.principles.${principle}`) }}
-        </dt>
-        <dd>{{ t('report.criteriaMet', { conforming: data.conforming[principle], total: data.totals[principle] }) }}</dd>
-      </div>
-    </dl>
+    <table class="w-full text-sm text-left">
+      <caption class="sr-only">
+        {{ t('report.resultsPerPrinciple') }}
+      </caption>
+      <thead>
+        <tr class="border-b border-gray-200 dark:border-gray-800">
+          <th scope="col" class="py-2 pr-4 font-medium text-gray-950 dark:text-white">
+            {{ t('report.principle') }}
+          </th>
+          <th scope="col" class="py-2 font-medium text-gray-950 dark:text-white">
+            {{ t('report.result') }}
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="principle in principles"
+          :key="principle"
+          class="border-b border-gray-100 dark:border-gray-900"
+        >
+          <td class="py-2 pr-4 text-gray-700 dark:text-gray-300">
+            {{ t(`report.principles.${principle}`) }}
+          </td>
+          <td class="py-2 text-gray-700 dark:text-gray-300">
+            {{ t('report.criteriaMet', { conforming: data.conforming[principle], total: data.totals[principle] }) }}
+          </td>
+        </tr>
+      </tbody>
+      <tfoot>
+        <tr class="border-t border-gray-200 dark:border-gray-800">
+          <th scope="row" class="py-2 pr-4 font-medium text-gray-950 dark:text-white">
+            {{ t('report.total') }}
+          </th>
+          <td class="py-2 font-medium text-gray-950 dark:text-white">
+            {{ t('report.criteriaMet', { conforming: data.conforming.all, total: data.totals.all }) }}
+          </td>
+        </tr>
+      </tfoot>
+    </table>
   </div>
 </template>
